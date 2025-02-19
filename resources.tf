558a788ca7e6a6c931d8ddd906393415dd5c3a65
mm-rafay <matt.morris@rafay.co>
2025-02-19 09:12:11 -0600

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -53,7 +53,6 @@ resource "rafay_workload" "private-chatbot" {
   }
   spec {
     namespace = "c-mm-xl"
-    version = "v11"
     placement {
       selector = "rafay.dev/clusterName=gpu-paas-demo-cluster"
     }
