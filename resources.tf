0b9b6467d581a21aa2bb729228d9102bc5ae5042
mm-rafay <matt.morris@rafay.co>
2025-02-18 20:27:00 -0600

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -61,7 +61,7 @@ resource "rafay_workload" "private-chatbot" {
       type = "Helm"
       artifact{
         values_paths {
-          name = "/home/terraform/app/scratch/job/values.yaml"
+          name = "~/values.yaml"
         }
         repository = "open-webui"
         chart_name = "open-webui"
